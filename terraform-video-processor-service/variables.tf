0d5af37ea0605dba67500ad066e78049af63502b
Léo Saglia <leosaglia@hotmail.com>
2025-07-09 19:57:59 -0300

    refactor: update SQS queue configurations and remove unused resources

--- terraform-video-processor-service/variables.tf
+++ terraform-video-processor-service/variables.tf
@@ -1,2 +1,7 @@
 # Todas as variáveis necessárias são obtidas via remote state
 # Não são necessárias variáveis adicionais
+variable "aws_bucket_name" {
+  description = "Nome do bucket S3 para armazenamento de vídeos e zips"
+  type        = string
+  default     = "fiap-video-bucket-20250706"
+}
--- terraform-video-queues/main.tf
+++ terraform-video-queues/main.tf
@@ -11,17 +11,37 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_sqs_queue" "uploaded_video_queue" {
-  name = "uploaded-video-queue"
-  delay_seconds = 0
-  max_message_size = 262144
+resource "aws_sqs_queue" "video_processing_queue" {
+  name                      = "video-processing-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
   message_retention_seconds = 86400
   receive_wait_time_seconds = 0
-  visibility_timeout_seconds = 30
+  visibility_timeout_seconds = 300
 
   tags = {
-    Name = "uploaded-video-queue"
+    Name        = "Video Processing Queue"
+    Environment = "production"
+    Service     = "video-processor"
   }
+}
+
+resource "aws_sqs_queue" "video_processing_dlq" {
+  name = "video-processing-dlq"
+
+  tags = {
+    Name        = "Video Processing Dead Letter Queue"
+    Environment = "production"
+    Service     = "video-processor"
+  }
+}
+
+resource "aws_sqs_queue_redrive_policy" "video_processing_queue" {
+  queue_url = aws_sqs_queue.video_processing_queue.id
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.video_processing_dlq.arn
+    maxReceiveCount     = 3
+  })
 }
 
 resource "aws_sqs_queue" "updated_video_processing_queue" {
